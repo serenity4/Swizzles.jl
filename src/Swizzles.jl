abfa53080f14a8a9be873e4c429545de22e2067e
serenity4 <cedric.bel@hotmail.fr>
2023-11-25 12:20:36 +0100

    Add swizzle macro

--- src/Swizzles.jl
+++ src/Swizzles.jl
@@ -22,6 +22,60 @@ construct_swizzle(::Type{T}, args::Tuple) where {T} = T(args...)
 construct_swizzle(::Type{Vector}, args::Tuple) = [args...]
 construct_swizzle(::Type{Vector{T}}, args::Tuple) where {T} = T[args...]
 
-export swizzle
+struct InvalidSwizzle <: Exception
+  msg::String
+end
+
+Base.showerror(io::IO, exc::InvalidSwizzle) = print(io, "InvalidSwizzle: ", msg)
+
+component_names_dict = Dict(
+  # [x, y, z, w] denoting a spatial 4-vector in homogeneous space.
+  'x' => 1,
+  'y' => 2,
+  'z' => 3,
+  'w' => 4,
+
+  # [r, g, b, a] denoting a color 4-vector with an alpha component.
+  'r' => 1,
+  'g' => 2,
+  'b' => 3,
+  'a' => 4,
+)
+
+if @isdefined(ScopedValue)
+  component_names = ScopedValue(component_names_dict)
+else
+  component_names = Ref(component_names_dict)
+end
+
+function generate_swizzle_expr(ex::Expr, T = nothing)
+  if !Meta.isexpr(ex, :., 2)
+    throw(ArgumentError("Expected expression of the form `<v>.<swizzle>`, got $(repr(ex))"))
+  end
+  v, swizzle = ex.args
+  if !isa(swizzle, QuoteNode)
+    throw(InvalidSwizzle("Expected `QuoteNode` value in `swizzle`, got value of type `$(typeof(swizzle))"))
+  end
+  swizzle = swizzle.value
+  if !isa(swizzle, Symbol)
+    throw(InvalidSwizzle("Expected symbol `QuoteNode` value in `swizzle`, got value of type `$(typeof(swizzle))"))
+  end
+  components = [component_names[][c] for c in string(swizzle)]
+  if isnothing(T)
+    :(swizzle($(esc(v)), $(components...)))
+  else
+    :(swizzle($(esc(T)), $(esc(v)), $(components...)))
+  end
+end
+
+macro swizzle(T, ex)
+  generate_swizzle_expr(ex, T)
+end
+
+macro swizzle(ex)
+  generate_swizzle_expr(ex)
+end
+
+export swizzle, @swizzle
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,23 +3,46 @@ using StaticArrays
 using Test
 
 @testset "Swizzles.jl" begin
-  v = [1, 2, 3]
-  @test swizzle(Tuple, v, 1, 3) === (1, 3)
-  sw = swizzle(v, 1, 3)
-  @test isa(sw, Vector{Int})
-  @test sw == [1, 3]
-  sw = swizzle(v, 1, 3, 3)
-  @test sw == [1, 3, 3]
+  @testset "`swizzle`" begin
+    v = [1, 2, 3]
+    @test swizzle(Tuple, v, 1, 3) === (1, 3)
 
-  v = @SVector [1.0, 2.0, 3.0]
-  sw = swizzle(v, 1, 3)
-  @test sw === @SVector [1.0, 3.0]
+    sw = swizzle(v, 1, 3)
+    @test isa(sw, Vector{Int})
+    @test sw == [1, 3]
 
-  v = @MVector [1.0, 2.0, 3.0]
-  sw = swizzle(v, 1, 3)
-  @test sw == @MVector [1.0, 3.0]
+    sw = swizzle(v, 1, 3, 3)
+    @test sw == [1, 3, 3]
 
-  v = SizedVector{3}([1, 2, 3])
-  sw = swizzle(v, 1, 3)
-  @test sw == SizedVector{2}([1.0, 3.0])
+    v = @SVector [1.0, 2.0, 3.0]
+    sw = swizzle(v, 1, 3)
+    @test sw === @SVector [1.0, 3.0]
+
+    v = @MVector [1.0, 2.0, 3.0]
+    sw = swizzle(v, 1, 3)
+    @test sw == @MVector [1.0, 3.0]
+
+    v = SizedVector{3}([1, 2, 3])
+    sw = swizzle(v, 1, 3)
+    @test sw == SizedVector{2}([1.0, 3.0])
+
+    sw = swizzle(v, 3)
+    @test sw === 3
+  end
+
+  @testset "`@swizzle`" begin
+    v = [1, 2, 3, 4]
+
+    @test (@eval @swizzle v.xzw) == [1, 3, 4]
+    @test (@eval @swizzle v.rab) == [1, 4, 3]
+    @test (@eval @swizzle v.xyx) == [1, 2, 1]
+    @test (@eval @swizzle v.zz) == [3, 3]
+    @test (@eval @swizzle v.zz) == [3, 3]
+
+    @test (@eval @swizzle Tuple v.xzw) === (1, 3, 4)
+    @test (@eval @swizzle Tuple v.rab) === (1, 4, 3)
+    @test (@eval @swizzle Tuple v.xyx) === (1, 2, 1)
+    @test (@eval @swizzle Tuple v.zz) === (3, 3)
+    @test (@eval @swizzle Tuple v.zz) === (3, 3)
+  end
 end;
